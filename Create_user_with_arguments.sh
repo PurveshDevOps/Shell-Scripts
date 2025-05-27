1ea91370c31270d7df42afa3402013e19bc8844e
Purvesh-DevOps <purveshdevops2025@gmail.com>
2025-05-27 07:35:34 +0200

    Create Create_user_with_arguments.sh

---
+++ Create_user_with_arguments.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+<<info
+
+this scripts shows how to add user with arguments
+info
+
+echo "Mohit: Hello $1"
+
+echo "Purvesh: Hii $2"
+
+echo "Purvesh: How are you doing $2"
+
+echo "Mohit: I am fine $1, what about you"
+
+echo "Purvesh: I am good too $2....Thanks"
