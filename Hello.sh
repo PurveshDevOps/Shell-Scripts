e34223833964863f421f1f6d257a81c9d2605f7c
Purvesh-DevOps <purveshdevops2025@gmail.com>
2025-05-27 07:32:15 +0200

    Create Hello.sh

---
+++ Hello.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# this is sholay by Purvesh
+
+<<info
+this is a multiline comment
+info
+
+echo "Gabbar: Kinte aadmi the"
+echo "sambha: 2 sarkar"
+echo "kitna baja hai: "
+
+date
+
+echo "kitne baje se class chal rahi hai: "
+
+uptime
