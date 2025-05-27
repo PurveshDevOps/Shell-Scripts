8d28b29c1bdcb9782bbb431ba36db8cd3c125b37
Purvesh-DevOps <purveshdevops2025@gmail.com>
2025-05-27 07:33:59 +0200

    Create variables.sh

---
+++ variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+<<info
+this is a demo for variables
+info
+
+read -p "Enter the name: " name
+
+echo "My name is $name"
+
+read -p "WHat is your name? : " name2
+
+echo "My name is $name2"
